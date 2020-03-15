e1531af2087bdea48e7c3c8014bad2c13a612a87
Zane Rankin <zwrankin@gmail.com>
2020-03-15 11:34:03 -0500

    add TreeCanopyCover class

---
+++ tree_canopy_cover/tree_canopy_cover.py
@@ -0,0 +1,116 @@
+import numpy as np
+import geopandas as gpd
+import rasterio
+from rasterio import warp
+from rasterio import windows
+import affine
+from shapely.geometry import Polygon
+import logging
+
+
+class TreeCanopyCover:
+    def __init__(self, filepath, resolution=1,
+                min_lat=None, max_lat=None, min_long=None, max_long=None):
+        self.src = rasterio.open(filepath)
+        self.min_lat = min_lat
+        self.max_lat = max_lat
+        self.min_long = min_long
+        self.max_long = max_long
+        
+        self.resolution = resolution
+        self.window = self._calc_window()
+        
+        if min_lat is None: 
+            self.transform_window = self.src.transform
+            self.band = self.src.read(1)    
+        else: 
+            raise UserWarning('there are bugs in windowed reading!')
+            self.transform_window = windows.transform(self.window,self.src.transform)
+            self.band = self.src.read(1, window=self.window)
+        
+        self.transform, self.width, self.height = self._calc_transform()
+        self.array = self._reproject()
+        
+    def _calc_window(self):
+        """Transform the specified lat/long into the row/col window"""
+        if self.min_lat is None: 
+            window = windows.Window(0, 0, self.src.shape[1], self.src.shape[0]) 
+        else: 
+            logging.warn('there are bugs in windowed transformation!')
+            # TODO - I think the width and height of the window are switched?
+            xs, ys = warp.transform({'init': 'epsg:4326'}, self.src.crs, [self.min_long, self.max_long], [self.max_lat, self.min_lat])
+            top_left = (xs[0], ys[0]) * ~self.src.transform
+            bottom_right = (xs[1], ys[1]) * ~self.src.transform
+            width = bottom_right[1] - top_left[1] 
+            height = bottom_right[0] - top_left[0]
+            # HACK - the first *should* be correct, but I had to switch them to get it to work...
+            # window = Window(top_left[0], top_left[1], width, height)
+            window = windows.Window(top_left[0], top_left[1], height, width)
+            # DEBUGGING
+#             x, y = top_left * self.src.transform 
+#             longs, lats = warp.transform(self.src.crs, {'init': 'epsg:4326'}, [x], [y])
+#             top_left_latlong = (lats[0], longs[0])
+        return window
+        
+    def _calc_transform(self):
+        bbox = windows.bounds(self.window, self.transform_window)
+        transform,width,height = warp.calculate_default_transform(self.src.crs, {"init":"epsg:4326"},
+                                                                  self.window.width,self.window.height,
+                                                                  left=bbox[0],bottom=bbox[1],
+                                                                  right=bbox[2],top=bbox[3], 
+                                                                 resolution=self.resolution)
+        return transform,width,height
+        
+    def _reproject(self):
+        out_array = np.ndarray((1, self.height, self.width))
+        warp.reproject(self.band,
+                   out_array,src_crs=self.src.crs,dst_crs={"init":"epsg:4326"},
+                   src_transform=self.transform_window,
+                   dst_transform=self.transform)
+        return out_array
+    
+    def to_gpdf(self):
+        to_gpdf(self.array[0], self.transform)
+
+    def save_tif(self, filepath):
+        profile = self.src.profile
+        profile['crs'] = rasterio.crs.CRS({"init":"epsg:4326"})
+        profile['transform'] = tcc.transform
+        profile['height'] = tcc.array.shape[1]
+        profile['width'] = tcc.array.shape[2]
+        with rasterio.Env():
+            # Write an array as a raster band to a new 8-bit file
+            with rasterio.open(filepath, 'w', **profile) as dst:
+                dst.write(self.array.astype(rasterio.uint8))
+
+
+def to_gpdf(array: np.array, transform: affine.Affine):
+    """There's gotta be a better way of doing this"""
+    assert len(array.shape) == 2, 'only 2D arrays supported'
+    resolution = transform.a
+    rows, cols = np.indices(array.shape)  
+    geoms = []
+    vals = []
+    for col,row in zip(np.ravel(cols), np.ravel(rows)):
+        # NOTE - top_left is x,y
+        top_left = transform * (col,row)
+        latitude = top_left[1]
+        longitude = top_left[0]
+
+        geoms += [Polygon([top_left, 
+                           (top_left[0]+resolution, top_left[1]),
+                           (top_left[0]+resolution, top_left[1]-resolution),
+                           (top_left[0], top_left[1]-resolution)])]
+        # NOTE - out_array is index by (row,col) wherease it is transformed by (col,row)
+        vals += [array[(row, col)]]
+
+    gpdf = gpd.GeoDataFrame(vals, geoms, columns=['tcc'])
+    gpdf.index.name = 'geometry'
+    gpdf = gpdf.reset_index()
+    return gpdf
+
+def save_tif_files():
+    tcc = TreeCanopyCover('data/raw/usfs_2016_CONUS_canopy_analytical_12-14-2018_u8.img', resolution=0.1)
+    tcc.save_tif('data/tcc_10km_resolution.tif')
+    tcc = TreeCanopyCover('data/raw/usfs_2016_CONUS_canopy_analytical_12-14-2018_u8.img', resolution=0.01)
+    tcc.save_tif('data/tcc_1km_resolution.tif')
